5b086139079f76aa9d81c6595a4f4ce0000f6bf5
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-14 03:41:25 +0000

    added cors for aquiring obects for front end

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const app   = express();
 const port  = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+var cors = require('cors');
+app.use(cors())
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
